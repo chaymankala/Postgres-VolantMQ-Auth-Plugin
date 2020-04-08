06d2df9109eb0b8e4087e203b559d24cd55d769d
chaymankala <chaithanya.mankala@gmail.com>
2020-04-09 00:24:39 +0530

    [update] ACL

--- auth.go
+++ auth.go
@@ -44,3 +44,38 @@ func (p *authProvider) Finduser(username string, password string) (user UserMode
 	}
 	return user, nil
 }
+
+func (p *authProvider) Password(clientID, username, password string) error {
+	_, err := p.Finduser(username, password)
+	if err != nil {
+		return vlauth.StatusDeny
+	}
+	return vlauth.StatusAllow
+}
+
+func (p *authProvider) FindUserByUsername(username string) (user UserModel, err error) {
+	db_query := fmt.Sprintf("select Username,Password,SubscriptionList,PublishList FROM %s WHERE username = $1", p.cfg.postgresUserTable)
+	err := p.connection.Query(db_query, username).Scan(&user.Username, &user.Password, &user.SubscriptionList, &user.PublishList)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (p *authProvider) ACL(clientID, username, topic string, access vlauth.AccessType) error {
+	user, err := p.FindUserByUsername(username)
+	if err != nil {
+		return vlauth.StatusDeny
+	}
+	permission := access.Type()
+	allowed := false
+	if permission == "write" {
+		allowed = IsTopicAllowed(topic, user.PublishList)
+	} else {
+		allowed = IsTopicAllowed(topic, user.SubscriptionList)
+	}
+	if err == nil && allowed {
+		return vlauth.StatusAllow
+	}
+	return vlauth.StatusDeny
+}
